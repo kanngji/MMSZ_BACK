39472aef84b2efea6855981a164f54111cf14f55
kanngji <50470748+kanngji@users.noreply.github.com>
2022-10-26 23:59:41 +0900

    jwt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,36 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+// 토큰 생성하기
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 // login user
 const loginUser = async (req, res) => {
-  res.json({ messg: "login user" });
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+
+    // create a token
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 // signup user
 const signupUser = async (req, res) => {
-  const { nickname, email, password } = req.body;
+  const { nickname, email, password, isAdmin } = req.body;
 
   try {
-    const user = await User.signup(nickname, email, password);
-    res.status(200).json({ email, user });
+    const user = await User.signup(nickname, email, password, isAdmin);
+
+    // create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 // bcrypt 사용하기 해쉬 비번
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
@@ -20,10 +21,32 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+    require: true,
+  },
 });
 
 // static signup method
-userSchema.statics.signup = async function (nickname, email, password) {
+userSchema.statics.signup = async function (
+  nickname,
+  email,
+  password,
+  isAdmin
+) {
+  // validation
+  if (!nickname || !email || !password) {
+    throw Error("All fields must be filled");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+  // password 강화 해야함 ex)abcABC123!
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password not strong enough");
+  }
+
   const exists = await this.findOne({ email });
 
   if (exists) {
@@ -33,7 +56,27 @@ userSchema.statics.signup = async function (nickname, email, password) {
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
   // 비밀번호에 hash 뭍히기
-  const user = await this.create({ nickname, email, password: hash });
+  const user = await this.create({ nickname, email, password: hash, isAdmin });
+  return user;
+};
+
+//static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+  if (!user) {
+    throw Error("Incorrect email");
+  }
+  // hashed password 와 user.password가 같은지 확인
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Incorrect password");
+  }
+
   return user;
 };
 
