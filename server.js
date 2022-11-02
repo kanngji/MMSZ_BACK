776a4019253b8a88ef63c78cf8af1645e0c82c82
xenosign <xenosign@naver.com>
2022-11-02 12:53:04 +0900

    env 적용

--- server.js
+++ server.js
@@ -2,20 +2,16 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
-
 // routes 의 boards를 가져옵니다
 const boardRoutes = require("./routes/boards");
 const userRoutes = require("./routes/user");
 const eventRoutes = require("./routes/event");
-const productRoutes = require("./routes/product");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 // middle ware
 app.use(express.json());
-app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
@@ -26,12 +22,9 @@ app.use((req, res, next) => {
 app.use("/api/boards", boardRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/events", eventRoutes);
-app.use("/api/product", productRoutes);
 // connet to db
 mongoose
-  .connect(
-    "mongodb+srv://tetz:qwer1234@cluster0.sdiakr0.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => {
     // db연결이 되어야지 서버가 열립니다.
     app.listen(PORT, () => {
