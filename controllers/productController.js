ed21c411f7e4a78124d20fdee9a119f3a52b1c1f
Victory-soo <victory-soo@naver.com>
2022-11-02 20:23:59 +0900

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,28 +4,32 @@ const data = require("../src/data");
 
 // push all product
 const pushAllProduct = async (req, res) => {
-  let result;
-  for (let i = 0; i < data.length; i++) {
-    result = await Product.collection.insertOne(data[i]);
-  }
-  res.status(200).json(result);
+    let result;
+    for (let i = 0; i < data.length; i++) {
+        result = await Product.collection.insertOne(data[i]);
+    }
+    res.status(200).json(result);
 };
 
 // get all product
 const getAllProduct = async (req, res) => {
-  const data = await Product.find({}).sort({ createdAt: -1 });
-  res.status(200).json(data);
+    const data = await Product.find({}).sort({ createdAt: -1 });
+    console.log(data);
+    res.status(200).json(data);
 };
 
 // write a review
 const writeReivew = async (req, res) => {
-  console.log(req.body);
-  const result = await Product.collection.updateOne({ _id: req.body._id }, { $addToSet: { review: req.body.review }, $set: { rating: req.body.avg, numReviews: req.body.numReviews } });
-  res.status(200).json(result);
+    console.log(req.body);
+    const result = await Product.collection.updateOne(
+        { _id: req.body._id },
+        { $addToSet: { review: req.body.review }, $set: { rating: req.body.avg, numReviews: req.body.numReviews } }
+    );
+    res.status(200).json(result);
 };
 
 module.exports = {
-  getAllProduct,
-  pushAllProduct,
-  writeReivew,
+    getAllProduct,
+    pushAllProduct,
+    writeReivew,
 };
